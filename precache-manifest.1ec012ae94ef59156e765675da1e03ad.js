fef8f73746b2557fa46a5943ea423f6d0f627403
Clément Berthou <clement.berthou.ext@francetv.fr>
2019-06-17 17:10:57 +0200

    Updates

---
+++ precache-manifest.1ec012ae94ef59156e765675da1e03ad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "055ff7a62f39eb2100af1a183d840900",
+    "url": "/revealjs-react-boilerplate/index.html"
+  },
+  {
+    "revision": "209f8a39efcb1426ae82",
+    "url": "/revealjs-react-boilerplate/static/css/2.f5eca524.chunk.css"
+  },
+  {
+    "revision": "2a3b7838c5ce3296b030",
+    "url": "/revealjs-react-boilerplate/static/css/main.4c1dfd73.chunk.css"
+  },
+  {
+    "revision": "209f8a39efcb1426ae82",
+    "url": "/revealjs-react-boilerplate/static/js/2.7cc6788c.chunk.js"
+  },
+  {
+    "revision": "2a3b7838c5ce3296b030",
+    "url": "/revealjs-react-boilerplate/static/js/main.da008c67.chunk.js"
+  },
+  {
+    "revision": "c689772ec3e79b2f00e5",
+    "url": "/revealjs-react-boilerplate/static/js/runtime~main.92fa9c8a.js"
+  },
+  {
+    "revision": "91295fa87df918411b49b7531da5d558",
+    "url": "/revealjs-react-boilerplate/static/media/league-gothic.91295fa8.ttf"
+  },
+  {
+    "revision": "9900a4643cc63c5d8f969d2196f72572",
+    "url": "/revealjs-react-boilerplate/static/media/league-gothic.9900a464.eot"
+  },
+  {
+    "revision": "cd382dc8a9d6317864b5810a320effc5",
+    "url": "/revealjs-react-boilerplate/static/media/league-gothic.cd382dc8.woff"
+  }
+]);
